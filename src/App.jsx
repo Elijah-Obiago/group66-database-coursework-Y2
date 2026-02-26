4829e7dbde27b6b0c10b8e5fa299c300315d2dcb
Elijah Obiago <obiagoelijah@gmail.com>
2026-02-26 14:44:29 +0000

    W1: NavBar, Footer, Main + design PT1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import "./App.scss";
 
 function App() {
+  const loggedInUser = "User";
   return (
     <div className="layout">
       <header>
         <h1>Travel Jab Clinic</h1>
         <p className="welcome">
-          Welcome to our travel vaccination clinic, User!
+          Welcome to our travel vaccination clinic, {loggedInUser}!
         </p>
       </header>
+
+      <nav>
+        <div className="navItem">
+          <a to="/modules">Home</a>
+        </div>
+        <div className="navItem">
+          <a to="/modules">Book Appointment</a>
+        </div>
+        <div className="navItem">
+          <a to="/modules">My Bookings</a>
+        </div>
+        <div className="navItem">
+          <a to="/modules">Prescriptions</a>
+        </div>
+        <div className="navItem">
+          <a to="/modules">Settings</a>
+        </div>
+      </nav>
+
+      <main>
+        <p>Homepage</p>
+      </main>
+
+      <footer>
+        <p className="Contact">Contact Us</p>
+      </footer>
     </div>
   );
 }
